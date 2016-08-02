e6bc9c057aaf093f3f5c95823cec6a3fe3a70b0a
Ted Collins <collins.ted@gmail.com>
2016-08-02 19:52:48 -0400

    Fixing production configuration

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module NoneOfTheAbove
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Eastern Time (US & Canada)'
-	if Rails.env.production?
+	if Rails.env.development?
 		config.web_console.whitelisted_ips = '10.211.55.2'
 	end
 
