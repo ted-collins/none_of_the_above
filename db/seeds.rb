3bb567d21d67fc602a9762995fefca63f3381ff6
Ted Collins <collins.ted@gmail.com>
2016-08-16 15:32:29 -0400

    Changing goal

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,13 +30,13 @@ end
 #Users
 User.where("email like 'dummy_%'").delete_all
 
-#if Rails.env.production?
+if Rails.env.production?
 	@states = {:CA=>383, :TX=>264, :NY=>196, :FL=>195, :IL=>128, :PA=>127, :OH=>115, :GA=>99, :MI=>98, :NC=>98, :NJ=>88, :VA=>82, :WA=>69, :MA=>66, :AZ=>66, :IN=>65, :TN=>64, :MO=>60, :MD=>59, :WI=>57, :MN=>54, :CO=>52, :AL=>48, :SC=>47, :LA=>46, :KY=>43, :OR=>39, :OK=>38, :CT=>35, :IA=>30, :MS=>29, :AR=>29, :UT=>29, :KS=>28, :NV=>27, :NM=>20, :NE=>18, :WV=>18, :ID=>16, :HI=>14, :ME=>13, :NH=>13, :RI=>10, :MT=>10, :DE=>9, :SD=>8, :AK=>7, :ND=>7, :DC=>6, :VT=>6, :WY=>5}
 	@max_i = 1
-#else
-#	@states = {:CA=>1, :TX=>1, :NY=>1, :FL=>1, :IL=>1, :PA=>1, :OH=>1, :GA=>1, :MI=>1, :NC=>1, :NJ=>1, :VA=>1, :WA=>1, :MA=>1, :AZ=>1, :IN=>1, :TN=>1, :MO=>1, :MD=>1, :WI=>1, :MN=>1, :CO=>1, :AL=>1, :SC=>1, :LA=>1, :KY=>1, :OR=>1, :OK=>1, :CT=>1, :IA=>1, :MS=>1, :AR=>1, :UT=>1, :KS=>1, :NV=>1, :NM=>1, :NE=>1, :WV=>1, :ID=>1, :HI=>1, :ME=>1, :NH=>1, :RI=>1, :MT=>1, :DE=>1, :SD=>1, :AK=>1, :ND=>1, :DC=>1, :VT=>1, :WY=>1}
-#	@max_i = 2
-#end
+else
+	@states = {:CA=>1, :TX=>1, :NY=>1, :FL=>1, :IL=>1, :PA=>1, :OH=>1, :GA=>1, :MI=>1, :NC=>1, :NJ=>1, :VA=>1, :WA=>1, :MA=>1, :AZ=>1, :IN=>1, :TN=>1, :MO=>1, :MD=>1, :WI=>1, :MN=>1, :CO=>1, :AL=>1, :SC=>1, :LA=>1, :KY=>1, :OR=>1, :OK=>1, :CT=>1, :IA=>1, :MS=>1, :AR=>1, :UT=>1, :KS=>1, :NV=>1, :NM=>1, :NE=>1, :WV=>1, :ID=>1, :HI=>1, :ME=>1, :NH=>1, :RI=>1, :MT=>1, :DE=>1, :SD=>1, :AK=>1, :ND=>1, :DC=>1, :VT=>1, :WY=>1}
+	@max_i = 1
+end
 
 cnt=0
 @states.each do |k,v|
