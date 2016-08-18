4a4cd2d9db52009b404b47b4834034023c4ef938
Ted Collins <collins.ted@gmail.com>
2016-08-18 18:01:36 -0400

    Fixing locale issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
  
 	def set_locale
-  		I18n.locale = current_user.try(:locale) || I18n.default_locale
-  		if I18n.locale.eql?(:en) && I18n.locale.eql?(:es)
-			 I18n.locale = :en
-		end
+  		I18n.locale ||= I18n.default_locale
+  		#if I18n.locale.eql?(:en) && I18n.locale.eql?(:es)
+		#	 I18n.locale = :en
+		#end
 	end
 
 end
