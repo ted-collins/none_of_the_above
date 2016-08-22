55cfd3a1ce172e20e3c1b81123dbc9930edcc221
Ted Collins <collins.ted@gmail.com>
2016-08-22 09:21:16 -0400

    Fixing user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,31 @@
 class UsersController < ApplicationController
   before_filter :sanitize_params
 
-  def set_locale
+  def set_user_locale
 	if(!params[:locale].nil? && !current_user.nil?)
 		current_user.locale = params[:locale]
 		current_user.save
 	end
-	redirect_to root_url
   end
 
   def opt_out
 	params.require(:email)
+	set_user_locale
 	@user = User.find_by_email(params[:email])
 	@err = nil
 	if(@user.nil?)
 		@err = "Could not locate email in database"
 	else
+		@user.skip_confirmation_notification!
+		@user.email = "#{@user.email}==OPTOUT"
 		@user.opt_out_at = DateTime.now
 		@status = @user.save
 	end
     respond_to do |format|
     	if(@err.nil?)
        		format.html { render template: "pages/opt_out" }
-			format.json  { render 'generic.json' }
 		else
 			format.html { render template: "pages/opt_out", :error => "Could not opt out #{current_user.errors.full_messages}" }
-			format.json  { render 'generic.json' }
 		end
     end
   end
