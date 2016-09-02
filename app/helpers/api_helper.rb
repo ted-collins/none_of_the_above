dfb156ff062a759e222ddbda0df32910d0ea9b5e
Ted Collins <collins.ted@gmail.com>
2016-09-01 21:21:18 -0400

    Updating goal

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -57,7 +57,7 @@ protected
 			arr_value_oth[i] = User.where("created_at <= '#{qu_start}' AND party_affiliation != 'democrat' AND party_affiliation != 'republican'").count
 			arr_date[i] = (DateTime.now - (19 - i).days).strftime("%b %d")
 			if( Rails.env.production?)
-				arr_value_goal[i] = 250000
+				arr_value_goal[i] = 300000
 			else
 				arr_value_goal[i] = 3000
 			end
